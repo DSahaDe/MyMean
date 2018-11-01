ef2faf393e84f2ea0d2dcd580c250eea9e57a929
Debomita Saha De <debomitasahade@Debomitas-MacBook-Air.local>
2018-11-01 08:46:55 +0530

    First

---
+++ NodeJS/myEvents.js
@@ -0,0 +1,19 @@
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+  console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+eventEmitter.on('scream',(a,b)=>{
+  setImmediate(()=>{
+  console.log("Called asynchronously");
+  });
+});
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
